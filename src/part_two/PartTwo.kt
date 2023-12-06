5eb9f876b53773b1d945a63271df0990ea9fb044
consultant <Philippe.cherlan-miche@techconsulting.tech>
2023-12-05 23:39:08 -0500

    day 01 part two challenge updated

--- src/part_two/PartTwo.kt
+++ src/part_two/PartTwo.kt
@@ -1,105 +1,35 @@
 package part_two
 
 import trebuchet.readFromFile
-import java.util.Collections
-
-
-// stringA.contains(stringB, ignoreCase = true)
-// split from a specific index
-// val substring = string.drop(string.indexOf('t')).take(7)
-// const address = '100 Queen St W, Toronto, ON M5H 2N2';
-// const civicNumber = address.substr(0, address.indexOf(' '));
-// val exist = map.containsValue(value)
-//  map.forEach { (key, value) -> println("$key = $value") }
-// println(string.last { it.isLetter() }) // n
 
 
 fun main(){
     val fileContent = readFromFile("/Users/consultant/Documents/Tasks/advent-of-code-2023/untitled/src/trebuchet/Trebuchet.txt")
-    val digitsMap = hashMapOf<String, Int>("One" to 1, "Two" to 2, "Three" to 3, "Four" to 4, "Five" to 5, "Six" to 6, "Seven" to 7, "Eight" to 8, "Nine" to 9)
-    var result = 0
-    var first = ""
-    var last = ""
-    var substringBeforeDigit = ""
-    var substringAfterDigit = ""
-    var letterDigits = hashMapOf<String, Int>()
-    var indexes = emptyList<Int>()
-    var sumofIndex = 0
 
+    var sum = 0
     fileContent.forEach { line ->
+        // Index, Digit
+        val digitsFound = mutableListOf<Pair<Int, Int>>()
 
-        try {
-            first = ""
-            first = line.first { it.isDigit() }.toString()
+        val digits = mapOf(
+            "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5,
+            "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9, "zero" to 0,
+            "1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5,
+            "6" to 6, "7" to 7, "8" to 8, "9" to 9, "0" to 0
+        )
 
-        }catch (e: Exception){
-            e.printStackTrace()
-        }
-
-
-        if (first != ""){
-            substringBeforeDigit = line.substringBefore(first)
-        }else{
-            substringBeforeDigit = line
-        }
-
-
-        if (substringBeforeDigit != "") {
-            digitsMap.forEach { key, value ->
-                if (substringBeforeDigit.contains(key, true)) {
-                    letterDigits.put(key,0)
-                    println("substringBeforeDigit: $substringBeforeDigit letterDigits: $letterDigits")
-                }
-
-
-                letterDigits.forEach { key, value ->
-
-                    indexes = substringBeforeDigit.indexesOf(key)
-
-                    for (i in indexes.indices){
-                        sumofIndex += indexes[i]
-                    }
-
-                //    letterDigits[key] = sumofIndex
-
-                }
-
-
+        for (digit in digits) {
+            if (line.indexOf(digit.key) != -1) {
+                digitsFound.add(Pair(line.indexOf(digit.key), digit.value))
+                digitsFound.add(Pair(line.lastIndexOf(digit.key), digit.value))
             }
         }
 
+        val sortedDigits = digitsFound.sortedBy { pair -> pair.first }
 
-        try {
-            last = line.last { it.isDigit() }.toString()
-        }catch (e: Exception){
-         e.printStackTrace()
-        }
-
-        if (last != ""){
-            substringAfterDigit = line.substringAfter(last)
-        }else{
-            substringAfterDigit = line
-        }
-
-
-        if (substringAfterDigit != "") {
-            digitsMap.forEach { key, value ->
-
-                if (substringAfterDigit.contains(key, true)) {
-                    //  println("substring: $substringAfterDigit, digit in letter format: $key")
-                    last = value.toString()
-                }
-            }
-        }
-        result += "$first$last".toInt()
+        sum += 10 * sortedDigits.first().second + sortedDigits.last().second
     }
+    println(sum)
+
 }
 
-
-
-public fun String?.indexesOf(substr: String, ignoreCase: Boolean = true): List<Int> {
-    return this?.let {
-        val regex = if (ignoreCase) Regex(substr, RegexOption.IGNORE_CASE) else Regex(substr)
-        regex.findAll(this).map { it.range.start }.toList()
-    } ?: emptyList()
-}
